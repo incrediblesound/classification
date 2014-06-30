1f1e26f56bcfa9da078020c692df4ab18f7972d9
James H. Edwards <james.howe.edwards@gmail.com>
2014-06-30 21:30:49 +0800

    initial commit

---
+++ categorize.js
@@ -0,0 +1,19 @@
+exports.data = [
+{cat: 'happy',txt:'Today was a wonderful day, I had a great lunch with my best friend and we enjoyed pleasant conversation from morning till night.'},
+{cat: 'happy',txt:'Today was my birthday! All my friends came to my house and we played fun games and sang songs. It was a great birthday, all of my favorite people were there.'},
+{cat: 'happy',txt:'I had a wonderful date with a sweet girl. We went to the movies and had dinner, and she said she had a great time.'},
+{cat: 'happy',txt:'I went to the beach with my friends today, it was so much fun. We ate snacks and went swimming.'},
+{cat: 'happy',txt:'I had a great time with my teacher today, we saw a wonderful exhibit in the museum. I really love modern art!'},
+{cat: 'sad',txt:'Today was terrible, it rained all day. There was nothing to do in the house, I was in a bad mood all day.'},
+{cat: 'sad',txt:'I had a horrible date today, we had dinner but the meat was bad. I got terrible gas pains and had to go home.'},
+{cat: 'sad',txt:'Today was so sad. My dog died. I loved him very much, I wanted to save him but there was nothing I could do.'},
+{cat: 'sad',txt:'I found out that I failed english and my grades are worse than I thought. I have to take a makeup class with a horrible teacher.'},
+{cat: 'sad',txt:'What a sad day, my best friend moved away. I feel like there is nothing to do no, nothing fun. It\'s a terrible feeling.'}
+];
+
+exports.tests = [
+'I had a terrible conversation with my mother today. Our relationship just keeps getting worse.',
+'I love my sister, she is wonderful. We always have so much fun together',
+'Today I made a big mistake, what a bad idea it was, and there\'s nothing I can do to fix it.',
+'Today was so much fun, I love my friends, they make me feel wonderful on a bad day.'
+]
